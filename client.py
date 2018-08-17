65fcb599e03c46ee38fce3cb50efbdb83c17c272
Ivan Itzcovich <i.itzcovich@gmail.com>
2018-08-17 17:57:41 -0300

    Update client

--- client.py
+++ client.py
@@ -10,6 +10,46 @@ from skimage.transform import resize
 
 from const import *
 
+class Extractor:
+
+    def __init__(self):
+        self.load()
+
+    def load(self):
+        self.sess = tf.Session()
+        ckpt_path = tf.train.latest_checkpoint("./models/")
+        saver = tf.train.import_meta_graph('{}.meta'.format(ckpt_path))
+        saver.restore(self.sess, ckpt_path)
+
+        g = tf.get_default_graph()
+
+        self.img = g.get_tensor_by_name("img:0")
+        self.training = g.get_tensor_by_name("training:0")
+        self.dim = g.get_tensor_by_name("dim:0")
+        self.prob = g.get_tensor_by_name("prob:0")
+        self.pred = g.get_tensor_by_name("pred:0")
+
+    def run(self, image):
+        img = resize(image, (SIZE, SIZE, SIZE))
+        img = (img / np.max(img))
+        img = np.reshape(img, [1, SIZE, SIZE, SIZE, 1])
+
+        prob = self.sess.run(self.prob, feed_dict={self.training: False, self.img: img})
+        return prob
+
 
 def run(model):
     pass
+
+
+if __name__ == "__main__":
+    img = nib.load(sys.argv[1])
+    img = img.get_fdata()
+
+    extractor = Extractor()
+    
+    prob = extractor.run(img)
+    brain = 1 * (prob > 0.5)
+
+    brain = nib.Nifti1Image(brain, np.eye(4))
+    nib.save(brain, "brain.nii")
