54db74bfafd5b3fb96bea0ab260f193b9ca5709e
Ivan Itzcovich <i.itzcovich@gmail.com>
2018-09-11 14:55:39 -0300

    Add bin script

--- deepbrain/extractor.py
+++ deepbrain/extractor.py
@@ -1,18 +1,12 @@
 import tensorflow as tf
-import os
-import sys
-import nibabel as nib
 import numpy as np
-import random
-import re
 from skimage.transform import resize
 
-
-from const import *
 
 class Extractor:
 
     def __init__(self):
+        self.SIZE = 128
         self.load()
 
     def load(self):
@@ -31,32 +25,12 @@ class Extractor:
 
     def run(self, image):
         shape = image.shape
-        img = resize(image, (SIZE, SIZE, SIZE))
+        img = resize(image, (self.SIZE, self.SIZE, self.SIZE))
         img = (img / np.max(img))
-        img = np.reshape(img, [1, SIZE, SIZE, SIZE, 1])
+        img = np.reshape(img, [1, self.SIZE, self.SIZE, self.SIZE, 1])
 
         prob = self.sess.run(self.prob, feed_dict={self.training: False, self.img: img}).squeeze()
         prob = resize(prob, (shape))
         return prob
 
 
-def run(model):
-    pass
-
-
-if __name__ == "__main__":
-    img = nib.load(sys.argv[1])
-    affine = img.affine
-    img = img.get_fdata()
-
-    extractor = Extractor()
-    
-    import time
-
-    now = time.time()
-    prob = extractor.run(img)
-    print(time.time() - now)
-    brain = 1 * (prob > 0.3)
-
-    brain = nib.Nifti1Image(brain, affine)
-    nib.save(brain, "brain.nii")
