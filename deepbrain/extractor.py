b27d9c935fc1d61b0921a7e5d69006b32c86b52e
Nick Boyarckin <70279602+nickboyar@users.noreply.github.com>
2023-12-04 16:38:28 +0400

    Update extractor.py for tf2

--- deepbrain/extractor.py
+++ deepbrain/extractor.py
@@ -3,8 +3,8 @@ import numpy as np
 from skimage.transform import resize
 import os
 
-PB_FILE = os.path.join(os.path.dirname(__file__), "models", "extractor", "graph_v2.pb")
-CHECKPOINT_DIR = os.path.join(os.path.dirname(__file__), "models", "extractor", "v2")
+PB_FILE = os.path.join(os.path.dirname(__file__), "models", "graph_v1.pb")
+CHECKPOINT_DIR = os.path.join(os.path.dirname(__file__), "models", "extractor", "v1")
 
 
 class Extractor:
@@ -15,9 +15,9 @@ class Extractor:
 
     def load_pb(self):
         graph = tf.Graph()
-        self.sess = tf.Session(graph=graph)
-        with tf.gfile.FastGFile(PB_FILE, 'rb') as f:
-            graph_def = tf.GraphDef()
+        self.sess = tf.compat.v1.Session(graph=graph)
+        with tf.io.gfile.GFile(PB_FILE, 'rb') as f:
+            graph_def = tf.compat.v1.GraphDef()
             graph_def.ParseFromString(f.read())
             with self.sess.graph.as_default():
                 tf.import_graph_def(graph_def)
